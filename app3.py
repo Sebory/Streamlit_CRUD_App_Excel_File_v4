aee2c72e8a166812fbcdd152d79146167df4e287
Sebory <diabykankan@gmail.com>
2023-06-04 23:37:08 +0000

    take off st watermark

--- app3.py
+++ app3.py
@@ -57,7 +57,47 @@ if side_bar == "Data Filter/Update":
         df2.loc[df_fruits_for_updt.index] = df_fruits_for_updt
         df2.to_excel("data/Fruits.xlsx", index=True)
     
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
+# ----------- LET MAKE PAGE FOOTER --------------
+import streamlit as st
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by Dr Diaby A. S.</p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
 
 
 
